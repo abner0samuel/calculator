53eeb091adb4fd626e01b07a81d113055ff21c57
abner0samuel <abaretabner@gmail.com>
2023-12-16 12:17:39 +0100

    simple calculator

---
+++ index.js
@@ -0,0 +1,67 @@
+const number1= document.querySelector('#one');
+const number2= document.querySelector('#two');
+const number3= document.querySelector('#three');
+const number4= document.querySelector('#four');
+const number5= document.querySelector('#five');
+const number6= document.querySelector('#six');
+const number7= document.querySelector('#seven');
+const number8= document.querySelector('#eight');
+const number9= document.querySelector('#nine');
+const number0= document.querySelector('#zero');
+const numbertext= document.querySelector('#text');
+const delate= document.querySelector('#delate');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector(#1');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector('#1');
+// const number1= document.querySelector('#1')
+
+
+number1.addEventListener("click",function(){
+    numbertext.value+=numbertext.value='1';
+
+})
+number2.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '2';
+
+})
+number3.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '3';
+
+})
+number4.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '4';
+
+})
+number5.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '5';
+
+})
+number6.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '6';
+
+})
+number7.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '7';
+
+})
+number8.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '8';
+
+})
+number9.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '9';
+
+})
+number0.addEventListener("click", function () {
+    numbertext.value+=numbertext.value = '0';
+
+})
+// number1.addEventListener("click", function () {
+//     numbertext.value = '1';
+
+// })
+
